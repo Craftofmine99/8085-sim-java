b949b7ebf96b967686e2e83b3fdf5c5a2d1321bc
Oliver Haney <ohaney@stetson.edu>
2018-12-20 15:06:54 -0500

    Commited what I think is the finished CPU_Register.java

--- model/CPU_Register.java
+++ model/CPU_Register.java
@@ -1,8 +1,69 @@
+/**
+ * Generic Register for 8085 CPU
+ * 
+ * @author haney-oliver
+ * @author whatever you want your name to be represented as
+ *
+ */
+
 public class CPU_Register
 {
-    public CPU_Register()
+	////////////
+	// Fields //
+	////////////
+	private String myName;
+	private boolean[] myByte;
+	
+	/////////////////
+	// Constructor //
+	/////////////////
+	/**
+	 * Instantiates a new null Register 
+	 * @param name
+	 */
+    public CPU_Register(String name)
     {
-
+    	myName = name;
+    	myByte = new boolean[8];
     }
-    private Bool[8] myByte;
+    
+    /**
+     * Instantiates a new Register with an immediate value 
+     * @param name
+     * @param immediateValue
+     */
+    public CPU_Register(String name, boolean[] immediateValue)
+    {
+    	myName = name;
+    	myByte = immediateValue;
+    }
+    
+    //////////////
+    // Behavior //
+    //////////////
+    /**
+     * Sets this Register's value
+     * @param value
+     */
+    public void setRegisterValue(boolean[] value) 
+    {
+    	myByte = value;
+    }
+    
+    /**
+     * @return myName
+     */
+    public String getName() 
+    {
+    	return myName;
+    }
+    
+    /**
+     * @return myByte
+     */
+    public boolean[] getCurrentValue() 
+    {
+    	return myByte;
+    }
+   
 }
