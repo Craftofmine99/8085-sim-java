265b088447822b3988c4343d0af1723b2ba73b52
Nicholas Gilmet <ngilmet1324@gmail.com>
2018-12-20 18:10:40 -0500

    corrected mem array size

--- src/model/Memory.java
+++ src/model/Memory.java
@@ -10,12 +10,12 @@ public class Memory
     /////////////////
     public Memory()
     {
-        memArray = new boolean[65536][8];
+        memArray = new boolean[4096][8];
     }
 
     public boolean[] getMemAtAddress(int address)
     {
-        if (address >= 0 && address < 65536)
+        if (address >= 0 && address < 4096)
             return memArray[address];
         else
             return new boolean[1];
