e220a9f6ead453e84f47825cd5efeb513cbee1ae
Nicholas Gilmet <ngilmet1324@gmail.com>
2018-12-22 18:34:32 -0500

    View Updated

--- src/view/View.java
+++ src/view/View.java
@@ -1,11 +1,28 @@
 package view;
 
 import java.awt.*;
+import java.lang.reflect.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class View
 {
+    private JTextArea myTextArea;
+    private JFrame myFrame;
+    private JButton checkButton;
+    private JButton runButton;
+    private JButton stepButton;
+    private ButtonListener checkListener;
+    private ButtonListener runButton;
+    private ButtonListener stepButton;
+
     View()
     {
-        
+        myFrame = new JFrame("8085 Simulator");
+        myFrame.setSize(600,500);
+        myTextArea = new JTextArea(30,80);
+
+
     }
 }
